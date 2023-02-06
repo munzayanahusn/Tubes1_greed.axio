ccca96061de30a5cbf6cad547821851f24ed88c8
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-02-06 21:40:55 +0700

    update key-value enums

--- src/main/java/Enums/ObjectTypes.java
+++ src/main/java/Enums/ObjectTypes.java
@@ -4,15 +4,15 @@ public enum ObjectTypes {
   PLAYER(1),
   FOOD(2),
   WORMHOLE(3),
-  GASCLOUD(4),
-  ASTEROIDFIELD(5),
-  TORPEDOSALVO(6),
+  GAS_CLOUD(4),
+  ASTEROID_FIELD(5),
+  TORPEDO_SALVO(6),
   SUPERFOOD(7),
-  SUPERNOVAPICKUP(8),
-  SUPERNOVABOMB(9),
+  SUPERNOVA_PICKUP(8),
+  SUPERNOVA_BOMB(9),
   TELEPORTER(10),
   SHIELD(11);
-  
+
   public final Integer value;
 
   ObjectTypes(Integer value) {
