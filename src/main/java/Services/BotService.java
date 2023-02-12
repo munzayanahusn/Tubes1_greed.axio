40403f0d16e9d9aa19417c44b437340842d7e1b2
munzayanahusn <88820186+munzayanahusn@users.noreply.github.com>
2023-02-12 15:53:32 +0700

    feat add target

--- src/main/java/Services/BotService.java
+++ src/main/java/Services/BotService.java
@@ -3,6 +3,7 @@ package Services;
 import Enums.*;
 import Models.*;
 
+import java.io.*;
 import java.util.*;
 import java.util.stream.*;
 
@@ -34,88 +35,80 @@ public class BotService {
     }
 
     public void computeNextPlayerAction(PlayerAction playerAction) {
+        Console console = System.console();
         playerAction.action = PlayerActions.FORWARD;
-        playerAction.heading = new Random().nextInt(360);
 
-        // GoTo Nearest Smaller Player
         if (!gameState.getGameObjects().isEmpty()) {
-            var playerList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.PLAYER)
-                    .sorted(Comparator
-                            .comparing(item -> getDistanceBetween(bot, item)))
-                    .filter(item -> getSmallerSize(item, bot))
+            // GetAway from Gas Cloud
+            var nearestObstacleList = gameState.getGameObjects()
+                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.GAS_CLOUD
+                            || item.getGameObjectType() == ObjectTypes.ASTEROID_FIELD
+                            || item.getGameObjectType() == ObjectTypes.WORMHOLE)
+                    .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item)))
                     .collect(Collectors.toList());
 
-            playerAction.heading = getHeadingBetween(playerList.get(0));
-        }
-
-        // Gather Nearest SuperFood
-        if (!gameState.getGameObjects().isEmpty()) {
-            var superFoodList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.SUPERFOOD)
+            var nearestFoodList = gameState.getGameObjects()
+                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.SUPERFOOD
+                            || item.getGameObjectType() == ObjectTypes.FOOD)
                     .sorted(Comparator
                             .comparing(item -> getDistanceBetween(bot, item)))
                     .collect(Collectors.toList());
 
-            playerAction.heading = getHeadingBetween(superFoodList.get(0));
-        }
-
-        // Gather Nearest Food
-        if (!gameState.getGameObjects().isEmpty()) {
-            var foodList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
-                    .sorted(Comparator
-                            .comparing(item -> getDistanceBetween(bot, item)))
+            var nearestPlayerList = gameState.getPlayerGameObjects()
+                    .stream().filter(item -> item.id != bot.id)
+                    .sorted(Comparator.comparing(item -> getDistanceBetween(bot, item)))
                     .collect(Collectors.toList());
 
-            playerAction.heading = getHeadingBetween(foodList.get(0));
+            // ACTION
+            // Out Of Boundaries
+
+            System.console().printf("------------------\nPlayer distance = " + getDistanceBetween(bot, nearestPlayerList.get(0)) + "\n");
+            System.console().printf("Player size = " + nearestPlayerList.get(0).getSize() + "\n");
+            System.console().printf("Bot size = " + bot.getSize() + "\n");
+            if (getDistanceFromCenter() + 2 * bot.getSize() > gameState.world.getRadius()) {
+                playerAction.heading = goToCenter();
+                System.console().printf("Get Away from Out Of Bound\n");
+            } else if (!nearestPlayerList.isEmpty() && (nearestPlayerList.get(0).getSize() > bot.getSize()) &&
+                    (getDistanceBetween(bot, nearestPlayerList.get(0)) <= (bot.getSize() * 1.5 + nearestPlayerList.get(0).getSize()))){
+                System.console().printf("Get Away from Nearest Bigger Player\n");
+                playerAction.heading = getHeadingAway(nearestPlayerList.get(0));
+            } else if (!nearestObstacleList.isEmpty() && (getDistanceBetween(bot, nearestObstacleList.get(0)) <= (bot.getSize()*2))) {
+                playerAction.heading = getHeadingAway(nearestObstacleList.get(0));
+                System.console().printf("Get Away from Nearest Obstacle\n");
+            } else {
+                System.console().printf("Targeting\n");
+                playerAction.heading = getHeadingBetween(setTarget(nearestFoodList, nearestPlayerList));
+            }
         }
-
-        // GetAway from Bigger Player
-        if (!gameState.getGameObjects().isEmpty()) {
-            var playerList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.PLAYER)
-                    .filter(item -> getBiggerSize(item, bot))
-                    .filter(item -> (getDistanceBetween(item, bot) < 2))
-                    .sorted(Comparator
-                            .comparing(item -> getDistanceBetween(bot, item)))
-                    .collect(Collectors.toList());
-
-            playerAction.heading = getHeadingAway(playerList.get(0));
-        }
-        // GetAway from Gas Cloud
-        if (!gameState.getGameObjects().isEmpty()) {
-            var playerList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.GAS_CLOUD)
-                    .filter(item -> (getDistanceBetween(item, bot) < 2))
-                    .collect(Collectors.toList());
-
-            playerAction.heading = getHeadingAway(playerList.get(0));
-        }
-
-        // GetAway from Asteroid Fields
-        if (!gameState.getGameObjects().isEmpty()) {
-            var playerList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.ASTEROID_FIELD)
-                    .filter(item -> (getDistanceBetween(item, bot) < 2))
-                    .collect(Collectors.toList());
-
-            playerAction.heading = getHeadingAway(playerList.get(0));
-        }
-
-        /*
-        if (!gameState.getGameObjects().isEmpty()) {
-            var foodList = gameState.getGameObjects()
-                    .stream().filter(item -> item.getGameObjectType() == ObjectTypes.FOOD)
-                    .sorted(Comparator
-                            .comparing(item -> getDistanceBetween(bot, item)))
-                    .collect(Collectors.toList());
-
-            playerAction.heading = getHeadingBetween(foodList.get(0));
-        }
-        */
-
         this.playerAction = playerAction;
+    }
+
+    public GameObject setTarget(List<GameObject> foodList, List<GameObject> playerList){
+        List<GameObject> enemyDistance;
+        GameObject retObject = null;
+        GameObject temp = null;
+        int i = 0;
+
+        if (playerList.get(0).getSize() <= bot.getSize()){
+            retObject = playerList.get(0);
+        }
+        if (!foodList.isEmpty()){
+            while(temp == null && i < foodList.size()) {
+                int finalI = i;
+                enemyDistance = playerList
+                        .stream().sorted(Comparator.comparing(item->getDistanceBetween(item, foodList.get(finalI))))
+                        .collect(Collectors.toList());
+                if (getDistanceBetween(enemyDistance.get(0), foodList.get(i)) < getDistanceBetween(bot, foodList.get(i))) {
+                    temp = foodList.get(i);
+                } else i++;
+            }
+            if (temp == null) temp = foodList.get(0);
+        }
+        if (retObject == null || getDistanceBetween(bot, temp) < getDistanceBetween(bot, retObject)) {
+            System.console().printf("Gather food\n");
+            retObject = temp;
+        } else System.console().printf("Go To Smaller Player\n");
+        return retObject;
     }
 
     public GameState getGameState() {
@@ -131,26 +124,29 @@ public class BotService {
         Optional<GameObject> optionalBot = gameState.getPlayerGameObjects().stream().filter(gameObject -> gameObject.id.equals(bot.id)).findAny();
         optionalBot.ifPresent(bot -> this.bot = bot);
     }
-
-    private boolean getSmallerSize(GameObject object1, GameObject object2){
-        return object1.getSize() < object2.getSize();
-    }
-    private boolean getBiggerSize(GameObject object1, GameObject object2){
-        return object1.getSize() > object2.getSize();
-    }
     private double getDistanceBetween(GameObject object1, GameObject object2) {
         var triangleX = Math.abs(object1.getPosition().x - object2.getPosition().x);
         var triangleY = Math.abs(object1.getPosition().y - object2.getPosition().y);
         return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
     }
 
+    private double getDistanceFromCenter() {
+        var triangleX = Math.abs(bot.getPosition().x - gameState.world.getCenterPoint().x);
+        var triangleY = Math.abs(bot.getPosition().y - gameState.world.getCenterPoint().y);
+        return Math.sqrt(triangleX * triangleX + triangleY * triangleY);
+    }
+
+    private int goToCenter() {
+        var direction = toDegrees(Math.atan2(gameState.world.getCenterPoint().y-bot.getPosition().y, gameState.world.getCenterPoint().x - bot.getPosition().x));
+        return (direction + 360) % 360;
+    }
     private int getHeadingBetween(GameObject otherObject) {
         var direction = toDegrees(Math.atan2(otherObject.getPosition().y - bot.getPosition().y,
                 otherObject.getPosition().x - bot.getPosition().x));
         return (direction + 360) % 360;
     }
     private int getHeadingAway(GameObject otherObject) {
-        var direction = toDegrees(Math.atan2(otherObject.getPosition().y + 90,
+        var direction = toDegrees(Math.atan2( otherObject.getPosition().y + 90,
                 otherObject.getPosition().x + 90));
         return (direction + 360) % 360;
     }
