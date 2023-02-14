45337c73af6c261a240ac825d02639cc0e94a221
bagas003 <bagasaryo03@gmail.com>
2023-02-14 15:03:26 +0700

    feat torpedo

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -63,7 +63,7 @@ public class Main {
         //This is a blocking call
         hubConnection.start().subscribe(() -> {
             while (hubConnection.getConnectionState() == HubConnectionState.CONNECTED) {
-                Thread.sleep(20);
+                Thread.sleep(13);
 
                 GameObject bot = botService.getBot();
                 if (bot == null) {
--- src/main/java/Models/GameObject.java
+++ src/main/java/Models/GameObject.java
@@ -10,6 +10,11 @@ public class GameObject {
   public Integer currentHeading;
   public Position position;
   public ObjectTypes gameObjectType;
+  public Integer effectsCode;
+  public Integer torpedoSalvoCount;
+  public Integer supernovaAvailable;
+  public Integer teleporterCount;
+  public Integer shieldCount;
 
   public GameObject(UUID id, Integer size, Integer speed, Integer currentHeading, Position position, ObjectTypes gameObjectType) {
     this.id = id;
@@ -18,6 +23,21 @@ public class GameObject {
     this.currentHeading = currentHeading;
     this.position = position;
     this.gameObjectType = gameObjectType;
+  }
+
+  public GameObject(UUID id, Integer size, Integer speed, Integer currentHeading, Position position, ObjectTypes gameObjectType, 
+  Integer effectsCode, Integer torpedoSalvoCount, Integer supernovaAvailable, Integer teleporterCount, Integer shieldCount) {
+    this.id = id;
+    this.size = size;
+    this.speed = speed;
+    this.currentHeading = currentHeading;
+    this.position = position;
+    this.gameObjectType = gameObjectType;
+    this.effectsCode = effectsCode;
+    this.torpedoSalvoCount = torpedoSalvoCount;
+    this.supernovaAvailable = supernovaAvailable;
+    this.teleporterCount = teleporterCount;
+    this.shieldCount = shieldCount;
   }
 
   public UUID getId() {
@@ -63,6 +83,10 @@ public class GameObject {
   public static GameObject FromStateList(UUID id, List<Integer> stateList)
   {
     Position position = new Position(stateList.get(4), stateList.get(5));
+    if (stateList.size() > 7) {
+      return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position, ObjectTypes.valueOf(stateList.get(3)),
+      stateList.get(6), stateList.get(7), stateList.get(8), stateList.get(9), stateList.get(10));
+    }
     return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position, ObjectTypes.valueOf(stateList.get(3)));
   }
 }
